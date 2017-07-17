16a51ef09c7b5946ee263ca189f250b9dcb6e470
vporta <vporta7@gmail.com>
2017-07-17 13:11:17 -0400

    made updates from 07/16/17 meeting

--- controllers/crowdsale.js
+++ controllers/crowdsale.js
@@ -11,12 +11,12 @@ router.get('/crowdsale', (req, res) => {
   res.render('crowdsale')
 })
 
-router.post('/subscribe', (req, res) => {
-  req.flash('info', 'Thank you for subscribing. Check your email inbox.')
-  Sendgrid(req.body.subscribe_email)
-  res.render('crowdsale', {
-    info: req.flash('info')
-  })
-})
+// router.post('/subscribe', (req, res) => {
+//   req.flash('info', 'Thank you for subscribing. Check your email inbox.')
+//   Sendgrid(req.body.subscribe_email)
+//   res.render('crowdsale', {
+//     info: req.flash('info')
+//   })
+// })
 
 module.exports = router
