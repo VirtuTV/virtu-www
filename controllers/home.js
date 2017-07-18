997fd8cdb8ace63033ddb573fb814da75ee4f850
vporta <vporta7@gmail.com>
2017-07-18 16:46:07 -0400

    added the whitepaper, edited the crowdsale page

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,18 @@
 // import express from 'express'
 const express = require('express')
-
+const fs = require('fs')
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.get('/whitepaper', (req, res) => {
+  const tempFile="./whitepaper.pdf";
+    fs.readFile(tempFile, function (err,data){
+       res.contentType("application/pdf");
+       res.send(data);
+    });
 })
 
 module.exports = router
